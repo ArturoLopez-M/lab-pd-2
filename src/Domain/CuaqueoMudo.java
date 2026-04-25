eb7e2a1e1fa3d109f118334c1cc83b6d85378f1b
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-04-25 11:13:15 -0500

    Creación e implementación de código en la clase CuaqueoMudo

---
+++ src/Domain/CuaqueoMudo.java
@@ -0,0 +1,13 @@
+package Domain;
+
+public class CuaqueoMudo implements Cuaqueable{
+
+    @Override
+    public void cuaquear() {
+        throw new UnsupportedOperationException("<< Silencio >>"); 
+    }
+
+        
+
+    
+}
